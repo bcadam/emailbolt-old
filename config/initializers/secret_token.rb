f7d8e6bb3be3599ee4e5b15729d8917b48b76415
Adam <adam.cragg@gmail.com>
2013-12-17 16:23:11 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emailbolt::Application.config.secret_key_base = '26248bf6ad480a808adebb1e3b91c5b42ba306bf065d48c67889b9e25ed57288bdbfe2b3d229ffa86f2922a158fcbe1cea13c5d9ee8bc3ef583f84d2f0639373'
