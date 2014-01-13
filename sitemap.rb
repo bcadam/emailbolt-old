da60e394410c782fe727e1e10d2203763754f03c
Adam <adam.cragg@gmail.com>
2014-01-13 16:13:41 -0500

    sitemap again

--- sitemap.rb
+++ sitemap.rb
@@ -4,8 +4,9 @@ require 'sitemap_generator'
 SitemapGenerator::Sitemap.default_host = 'http://www.emailbolt.com'
 SitemapGenerator::Sitemap.create do
   add '/home', :changefreq => 'daily', :priority => 0.9
-  add '/blog', :changefreq => 'weekly'
+  add '/blog', :changefreq => 'daily'
   add '/lookup', :changefreq => 'weekly'
   add '/faq', :changefreq => 'weekly'
+  add '/tos', :changefreq => 'weekly'
 end
 SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
