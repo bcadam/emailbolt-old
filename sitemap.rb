11286aa3650818baf37eea68f0ecf526c2b5f90b
Adam <adam.cragg@gmail.com>
2014-01-13 15:44:42 -0500

    seo changes

--- sitemap.rb
+++ sitemap.rb
@@ -4,6 +4,7 @@ require 'sitemap_generator'
 SitemapGenerator::Sitemap.default_host = 'http://www.emailbolt.com'
 SitemapGenerator::Sitemap.create do
   add '/home', :changefreq => 'daily', :priority => 0.9
+  add '/blog', :changefreq => 'weekly'
   add '/lookup', :changefreq => 'weekly'
   add '/faq', :changefreq => 'weekly'
 end
